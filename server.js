dfcc14821430b859742dfa552a4ea65c6746c60b
sparker416 <sparker416@gmail.com>
2016-10-08 15:27:56 -0400

    updated server.js to try to run on heroku

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ var cookieParser = require('cookie-parser');
 var session = require('express-session');
 var nodemailer = require('nodemailer');
 
+/*
 var connection_string = "127.0.0.1:27017/webdev2016";
 if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD){
     connection_string = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
@@ -15,12 +16,13 @@ if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD){
         process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
         process.env.OPENSHIFT_APP_NAME;
 }
-var db = mongoose.connect('mongodb://' + connection_string);
+*/
+//var db = mongoose.connect('mongodb://' + connection_string);
 
 var app = express();
 
-var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
-var port = process.env.OPENSHIFT_NODEJS_PORT || "3000";
+//var ipaddress = process.env. || "127.0.0.1";
+var port = process.env.PORT || "3000";
 
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json()); // for parsing application/json
@@ -28,7 +30,7 @@ app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x
 app.use(express.bodyParser({ uploadDir: './public/uploads', keepExtensions: true }));
 app.use(multer());
 //mongoose.connect('mongodb://localhost/cs4550');
-
+/*
 app.use(session({
     secret: process.env.SESSION_SECRET,
     resave: true,
@@ -37,11 +39,11 @@ app.use(session({
 app.use(cookieParser());
 app.use(passport.initialize());
 app.use(passport.session());
-// app.use(nodemailer());
+*/
 
 require('./public/project/server/app.js')(app, db, mongoose);
 
-app.listen(port, ipaddress);
+app.listen(port);
 
 /*
 var express = require('express');
